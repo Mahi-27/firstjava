ef24d790567b1445c6c8dad180e189edc4903c9e
Mahlet <Mahlet@MAHI.hsd1.dc.comcast.net>
2021-07-13 13:10:31 -0400

    new feafture add on the java class

--- Grade/src/pakage1/a.java
+++ Grade/src/pakage1/a.java
@@ -3,6 +3,7 @@ package pakage1;
 public class a {
 
 	public static void main(String[] args) {
+		// this is me
 		// TODO Auto-generated method stub
 System.out.println("grade project class a");
 	}
